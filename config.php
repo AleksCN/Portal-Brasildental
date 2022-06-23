ed476ef7943a9d78bd3383e8156566169ef82c75
AleksCN <107719099+AleksCN@users.noreply.github.com>
2022-06-22 22:31:45 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '62862055';
+    $dbName = 'db_user';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//    if($conexao->connect_errno)
+//    {
+//         echo "Erro";
+//    }
+//    else
+//    {
+//         echo "Conexão efetuada com sucesso";
+//    }
+?>
